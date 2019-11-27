3ee6fab75bfb18a5aa9a18cf25632f234edc82dc
Matthew Edge <MatthewEdge@users.noreply.github.com>
2019-11-27 07:21:36 -0500

    Create vim-plugins.sh

---
+++ vim-plugins.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Installs my preferred Vim plugins using vim8+
+git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/dist/start/vim-fugitive
