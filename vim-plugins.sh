43ac87183521b03b1c4742508685e2789fd66a41
Matthew Edge <matt.edge@levvel.io>
2020-02-12 15:32:15 -0500

    Add additional plugins. Working on function

--- vim-plugins.sh
+++ vim-plugins.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+# Usage: githubInstall user/repo
+githubInstall() {
+  REPO=$1
+  NAME=$(echo $REPO | sed 's/\(.*\)\/\(.*\)/\2/g')
+
+  echo "Installing $REPO to ~/.vim/pack/dist/start/$NAME"
+
+  echo "git clone https://github.com/$REPO.git ~/.vim/pack/dist/start/$NAME"
+}
+
 # Installs my preferred Vim plugins using vim8+
-git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
-git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
-git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/dist/start/vim-fugitive
-git clone https://github.com/christoomey/vim-tmux-navigator.git ~/.vim/pack/dist/start/vim-tmux-navigator
+githubInstall faith/vim-go
+githubInstall vim-airline/vim-airline
+githubInstall tpope/vim-fugitive
+githubInstall christoomey/vim-tmux-navigator
+githubInstall frazrepo/vim-rainbow
+githubInstall preservim/nerdcommenter
