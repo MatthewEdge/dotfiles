a1bf02903ce2716e0212e9710c2cae8d5d809cbd
Matthew Edge <mattedgeis@gmail.com>
2020-06-09 18:15:47 -0400

    Overhaul to use sudo for simplicity in copy vs install

--- archshell.sh
+++ archshell.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 # Configure Manjaro ArchLinux w/ i3 WM
-# Should be run with root priviledges
 
-pacman -Syy
+sudo pacman -Syy
 
 # Autostart NetworkManager
-systemctl enable NetworkManager
+sudo systemctl enable NetworkManager
 
 # Copy trackpad conf
 cp -f ./archlinux/etc/X11/xorg.conf.d/*.conf /etc/X11/xorg.conf.d/
@@ -18,40 +17,51 @@ cp -f ./.zshrc $HOME/.zshrc
 cp -f ./.vimrc $HOME/.vimrc
 
 # Git
-pacman -S git
+sudo pacman -S git
 git config --global user.name "Matthew Edge"
 git config --global user.email "mattedgeis@gmail.com"
 
 # Ensure packages
 
 # Font preference
-pacman -S otf-fira-code
+sudo pacman -S otf-fira-code
 cp -f ./archlinux/.Xresources $HOME/.Xresources
 
 # ZSH autosuggest
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-## Vim
-pacman -S gvim nodejs
+## Vim (need node for CoC)
+sudo pacman -S gvim nodejs
 vim +PlugInstall +qall
 
 ## Java/Scala
-pacman -S jdk-openjdk scala sbt
-vim +CocInstall coc-metals
+sudo pacman -S jdk-openjdk scala sbt
+vim +CocInstall coc-metals +qall
+
+## JS/TS
+vim +CocInstall coc-tsserver +qall
+
+## Docker
+sudo pacman -S docker
+sudo groupadd docker
+sudo usermod -aG docker $(whoami)
+newgrp docker
 
 # Misc. utilities
 
 ## Radeo Drivers
 # TODO necessary?
-pacman -S mesa libva-mesa-driver vulkan-radeon
+sudo pacman -S mesa libva-mesa-driver vulkan-radeon
 
 # Audo fix
 install_pulse
 
 # Zoom
 echo "Zoom Client time. Download the .tar.xz to ~/Downloads"
-open "https://zoom.us/download?os=linux"
-pacman -U $HOME/Downloads/zoom_x86_64.pkg.tar.xz
+echo "open https://zoom.us/download?os=linux"
+echo "When done - click enter..."
+read WaitForIt
+sudo pacman -U $HOME/Downloads/zoom_x86_64.pkg.tar.xz
 
 # Configuring default microphone
 echo "Grab microphone device id:"
@@ -59,8 +69,9 @@ pacmd list-sources | grep -e device.string -e 'name:'
 echo "Now paste this at the bottom of /etc/pulse/default.pa:"
 echo "set-default-source DEVICE-ID-HERE"
 
-echo "Probably a good idea to restart now!"
-
 # Screen capture
-pacman -S ffmpeg
+sudo pacman -S ffmpeg
 echo "ffmpeg capture aliases in zshrc"
+
+echo "Done!"
+echo "Probably a good idea to restart now!"
