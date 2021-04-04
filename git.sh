08dd9b347032610896735af22d4ca70d70003c02
Matthew Edge <medge@medgelabs.io>
2021-04-04 12:51:03 -0400

    Fix git to copy, not ln

--- git.sh
+++ git.sh
@@ -7,5 +7,5 @@ git config --global core.pager 'diff-so-fancy | less --tabs=4 -RFX'
 git config --global init.defaultBranch main
 git config --global pull.rebase false
 
-ln -s ./.config/git/.gitignore_global $HOME/.gitignore_global
+cp .gitignore_global $HOME/.gitignore_global
 git config --global core.excludesfile $HOME/.gitignore_global
