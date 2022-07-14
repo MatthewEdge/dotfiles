bd495142b08ee85e1c05dfe3b49f2de8c2558c88
medge <medge@medgelabs.io>
2022-07-14 05:26:48 -0400

    Update ssh setup

--- ssh.sh
+++ ssh.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 EMAIL=${1:-"medge@medgelabs.io"}
+EXPECTED_FILE=$HOME/.ssh/id_ed25519
 
-if [ ! -z "$HOME/.ssh/id_rsa" ]; then
+if [ ! -z "$EXPECTED_FILE" ]; then
   echo "SSH Key already present. Exiting.."
   exit 0
 fi
 
 # NOTE: `-q` silences the agent and `-N` sets an empty password
-ssh-keygen -t rsa -b 4096 -C $EMAIL -f $HOME/.ssh/id_rsa -q -N "${SSH_PASS}"
+ssh-keygen -t ed25519 -C $EMAIL -f $EXPECTED_FILE -q -N "${SSH_PASS}"
 
 # Ensure SSH agent is running
 eval "$(ssh-agent -s)"
 
-# Add key to
-ssh-add "$HOME/.ssh/id_rsa"
+# Add key to ssh-agent
+ssh-add "$EXPECTED_FILE"
