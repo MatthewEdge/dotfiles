5716155a1e50e74323341eabb6d6e5fe8f4c5f1a
Matthew Edge <medge@medgelabs.io>
2022-12-28 09:06:29 -0500

    Add dock minimize setting for ubuntu

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -62,3 +62,6 @@ rm -f awscli.zip
 # Reminder: key mapping
 echo "Install Gnome Tweaks and under Keyboard > Additional Layout Options > check 'Swap Left Win with Left Ctrl'"
 sudo apt install -y gnome-tweaks
+
+# Enable minimizing apps to Dock on dock icon click
+gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
--- ubuntu/README.md
+++ ubuntu/README.md
@@ -5,3 +5,9 @@
 The `/etc/udev/rules.d/90-block-webcam-mic.rules` file may need to be updated based
 on the output of `lsusb`. The `idVendor` and `idProduct` may need to be updated
 on new systems.
+
+Once changed, run:
+
+```bash
+sudo udevadm control --reload-rules
+```
