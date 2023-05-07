95af90ce07955bc8e879dbcf6d377428d7e4a035
Matthew Edge <medge@medgelabs.io>
2023-05-07 14:59:33 -0400

    Quick clone for server installations

---
+++ ubuntu-server-install.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# Variation of the Desktop install for Ubuntu Homelab setups
+
+set -ex
+
+ORIG=$(pwd)
+
+sudo apt update -y
+
+# Git / OpenSSH
+sudo apt install -y git
+./git.sh
+
+# Dev Dependencies and tooling
+sudo apt install -y \
+    htop \
+    fzf \
+    ripgrep \
+    xclip \
+    # for neovim
+    ninja-build gettext libtool-bin cmake g++ pkg-config unzip curl && \
+sudo apt remove -y vim
+
+if [ ! -d "$HOME/neovim" ]; then
+    git clone https://github.com/neovim/neovim.git $HOME/neovim
+else
+    cd $HOME/neovim
+    git pull
+    cd $ORIG
+fi
+
+cd $HOME/neovim
+rm -r build/ || true # clear the CMake cache
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make install
+# Ensure neovim config link
+rm -rf ~/.config/nvim
+ln -s $(pwd)/nvim ~/.config/nvim
+
+# OhMyZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cp ./.zshrc $HOME/.zshrc
