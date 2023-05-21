3ecdd433a9d13f30dbb02005f8bc733594edd89e
Matthew Edge <medge@medgelabs.io>
2023-05-21 19:46:16 -0400

    test-runner now working

--- nvim/lua/go-treesitter.lua
+++ nvim/lua/go-treesitter.lua
@@ -84,29 +84,29 @@ local M = {
      result: (type_identifier)@method.result
      body:(block)
      )@method.declaration)]],
-  query_test_func = [[((function_declaration name: (identifier) @test_name
+
+  -- Requires test_name be string.format()'d in on use
+  query_test_func = [[(
+    (function_declaration 
+        name: (identifier) @test_name
         parameters: (parameter_list
             (parameter_declaration
-                     name: (identifier)
-                     type: (pointer_type
-                         (qualified_type
-                          package: (package_identifier) @_param_package
-                          name: (type_identifier) @_param_name))))
-         ) @testfunc
-      (#contains? @test_name "Test")
-      (#match? @_param_package "testing")
-      (#match? @_param_name "T"))]],
+                name: (identifier)
+                type: (pointer_type
+                    (qualified_type
+                     package: (package_identifier) @_param_package
+                     name: (type_identifier) @_param_name))))
+    ) @testfunc
+    (#match? @_param_package "testing")
+    (#match? @_param_name "T")
+    (#eq? @test_name "%s")
+  )]],
   query_testcase_node = [[(literal_value (literal_element (literal_value .(keyed_element (literal_element (identifier)) (literal_element (interpreted_string_literal) @test.name)))))]],
   query_string_literal = [[((interpreted_string_literal) @string.value)]],
 }
 
 local function debug(...)
-    -- print(...)
-    -- local msg = ''
-    -- for _, v in ipairs(...) do
-        -- msg = msg .. tostring(v) .. '\t'
-    -- end
-    -- vim.notify(msg, vim.log.levels.DEBUG)
+    -- No-op now that we have sanity
 end
 
 -- go -> treesitter keyword
@@ -114,6 +114,35 @@ local function get_name_defaults()
   return { ['func'] = 'function', ['if'] = 'if', ['else'] = 'else', ['for'] = 'for' }
 end
 
+-- Returns the line number for the given test name in the given buffer
+M.get_test_line = function(bufnr, name)
+    if bufnr == nil then
+        print("bufnr is nil. Can't parse tree")
+        return -1
+    end
+
+    local formatted = string.format(M.query_test_func, name)
+    local query = vim.treesitter.query.parse("go", formatted)
+
+    -- Fetch the root tree of the buffer for querying
+    local parser = vim.treesitter.get_parser(bufnr, "go")
+    local tree = parser:parse()[1]
+    local root = tree:root()
+
+    for _, node in query:iter_captures(root, bufnr, 0, -1) do
+        local startRow, _, _, _ = vim.treesitter.get_node_range(node)
+        return startRow
+    end
+
+    -- If we get here - no node found
+    print("no line number found for test " .. name)
+    return -1
+end
+
+M.get_test_at_cursor = function(bufnr)
+end
+
+-- Return all nodes associated to cursor location
 M.nodes_at_cursor = function(query, default, bufnr, ntype)
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
   row, col = row, col + 1
@@ -185,261 +214,252 @@ M.get_interface_method_node_at_pos = function(bufnr)
 end
 
 M.get_func_method_node_at_pos = function(bufnr)
-  local query = M.query_func .. ' ' .. M.query_method_name
-  local bufn = bufnr or vim.api.nvim_get_current_buf()
+    local query = M.query_func .. ' ' .. M.query_method_name
+    local bufn = bufnr or vim.api.nvim_get_current_buf()
 
-  local ns = M.nodes_at_cursor(query, get_name_defaults(), bufn)
-  if ns == nil then
-    return nil
-  end
-  if ns == nil then
-    debug('function not found')
-  else
+    local ns = M.nodes_at_cursor(query, get_name_defaults(), bufn)
+    if ns == nil then
+        return nil
+    end
+
     return ns[#ns]
-  end
 end
 
 M.get_testcase_node = function(bufnr)
-  local query = M.query_testcase_node
-  local bufn = bufnr or vim.api.nvim_get_current_buf()
-  local ns = M.nodes_at_cursor(query, get_name_defaults(), bufn, 'name')
-  if ns == nil then
-    debug('test case not found')
-  else
-    debug('testcase node', ns[#ns])
-    return ns[#ns]
-  end
+    local query = M.query_testcase_node
+    local bufn = bufnr or vim.api.nvim_get_current_buf()
+    local ns = M.nodes_at_cursor(query, get_name_defaults(), bufn, 'name')
+    if ns == nil then
+        debug('test case not found')
+    else
+        debug('testcase node', ns[#ns])
+        return ns[#ns]
+    end
 end
 
 M.get_string_node = function(bufnr)
-  local query = M.query_string_literal
-  local bufn = bufnr or vim.api.nvim_get_current_buf()
-  local ns = M.nodes_at_cursor(query, get_name_defaults(), bufn, 'value')
-  if ns == nil then
-    debug('struct not found')
-  else
-    debug('struct node', ns[#ns])
-    return ns[#ns]
-  end
+    local query = M.query_string_literal
+    local bufn = bufnr or vim.api.nvim_get_current_buf()
+    local ns = M.nodes_at_cursor(query, get_name_defaults(), bufn, 'value')
+    if ns == nil then
+        debug('struct not found')
+    else
+        debug('struct node', ns[#ns])
+        return ns[#ns]
+    end
 end
 
 M.get_import_node_at_pos = function(bufnr)
-  -- local bufn = bufnr or vim.api.nvim_get_current_buf()
+    local cur_node = tsutil.get_node_at_cursor()
 
-  local cur_node = tsutil.get_node_at_cursor()
-
-  if cur_node and (cur_node:type() == 'import_spec' or cur_node:parent():type() == 'import_spec') then
-    return cur_node
-  end
+    if cur_node and (cur_node:type() == 'import_spec' or cur_node:parent():type() == 'import_spec') then
+        return cur_node
+    end
 end
 
 -- vim.treesitter.get_node_text only for nvim > 0.9
-M.get_node_text = function(...)
-    return vim.treesitter.get_node_text(...)
-end
+M.get_node_text = vim.treesitter.get_node_text
 
 M.get_module_at_pos = function(bufnr)
-  local node = M.get_import_node_at_pos(bufnr)
-  if node then
-    local module = M.get_node_text(node, vim.api.nvim_get_current_buf())
-    module = string.gsub(module, '"', '')
-    return module
-  end
+    local node = M.get_import_node_at_pos(bufnr)
+    if node then
+        local module = M.get_node_text(node, vim.api.nvim_get_current_buf())
+        module = string.gsub(module, '"', '')
+        return module
+    end
 end
 
 M.get_package_node_at_pos = function(bufnr)
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  row, col = row, col + 1
-  if row > 10 then
-    return
-  end
-  local query = M.query_package
-  local bufn = bufnr or vim.api.nvim_get_current_buf()
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+    row, col = row, col + 1
+    if row > 10 then
+        return
+    end
+    local query = M.query_package
+    local bufn = bufnr or vim.api.nvim_get_current_buf()
 
-  local ns = M.nodes_at_cursor(query, get_name_defaults(), bufn)
-  if ns == nil then
-    return nil
-  end
-  if ns == nil then
-    debug('package not found')
-  else
-    return ns[#ns]
-  end
+    local ns = M.nodes_at_cursor(query, get_name_defaults(), bufn)
+    if ns == nil then
+        return nil
+    end
+    if ns == nil then
+        debug('package not found')
+    else
+        return ns[#ns]
+    end
 end
 
 M.in_func = function()
-  local current_node = tsutil.get_node_at_cursor()
-  if not current_node then
-    return false
-  end
-  local expr = current_node
-
-  while expr do
-    if expr:type() == 'function_declaration' or expr:type() == 'method_declaration' then
-      return true
+    local current_node = tsutil.get_node_at_cursor()
+    if not current_node then
+        return false
     end
-    expr = expr:parent()
-  end
-  return false
+    local expr = current_node
+
+    while expr do
+        if expr:type() == 'function_declaration' or expr:type() == 'method_declaration' then
+            return true
+        end
+        expr = expr:parent()
+    end
+    return false
 end
 
 local nodes = {}
 local nodestime = {}
 M.get_all_nodes = function(query, lang, defaults, bufnr, pos_row, pos_col, ntype)
-  debug(query, lang, defaults, pos_row, pos_col)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-  local key = tostring(bufnr) .. query
-  local filetime = vim.fn.getftime(vim.fn.expand('%'))
-  if nodes[key] ~= nil and nodestime[key] ~= nil and filetime == nodestime[key] then
-    return nodes[key]
-  end
-  -- ulog(bufnr, nodestime[key], filetime)
-  -- todo a huge number
-  pos_row = pos_row or 30000
-  local success, parsed_query = pcall(function()
-    return vim.treesitter.query.parse(lang, query)
-  end)
-  if not success then
-    debug('failed to parse ts query: ' .. query .. 'for ' .. lang)
-    return nil
-  end
-
-  local parser = require('nvim-treesitter.parsers').get_parser(bufnr, lang)
-  local root = parser:parse()[1]:root()
-  local start_row, _, end_row, _ = root:range()
-  local results = {}
-  local node_type
-  for match in require('nvim-treesitter.query').iter_prepared_matches(parsed_query, root, bufnr, start_row, end_row) do
-    local sRow, sCol, eRow, eCol
-    local declaration_node
-    local type_node
-    local type = ''
-    local name = ''
-    local op = ''
-    debug(match)
-
-    require('nvim-treesitter.locals').recurse_local_nodes(match, function(_, node, path)
-      -- local idx = string.find(path, ".", 1, true)
-      -- The query may return multiple nodes, e.g.
-      -- (type_declaration (type_spec name:(type_identifier)@type_decl.name type:(type_identifier)@type_decl.type))@type_decl.declaration
-      -- returns { { @type_decl.name, @type_decl.type, @type_decl.declaration} ... }
-      local idx = string.find(path, '.[^.]*$') -- find last `.`
-      op = string.sub(path, idx + 1, #path)
-      local a1, b1, c1, d1 = vim.treesitter.get_node_range(node)
-      local dbg_txt = M.get_node_text(node, bufnr) or ''
-      if #dbg_txt > 100 then
-        dbg_txt = string.sub(dbg_txt, 1, 100) .. '...'
-      end
-      type = string.sub(path, 1, idx - 1) -- e.g. struct.name, type is struct
-      if type:find('type') and op == 'type' then -- type_declaration.type
-        node_type = M.get_node_text(node, bufnr)
-        debug('type: ' .. type)
-      end
-
-      -- stylua: ignore
-      debug(
-        "node ", vim.inspect(node), "\n path: " .. path .. " op: " .. op
-          .. "  type: " .. type .. "\n txt: " .. dbg_txt .. "\n range: " .. tostring(a1 or 0)
-          .. ":" .. tostring(b1 or 0) .. " TO " .. tostring(c1 or 0) .. ":" .. tostring(d1 or 0)
-      )
-      -- stylua: ignore end
-      --
-      -- may not handle complex node
-      if op == 'name' or op == 'value' or op == 'definition' then
-        debug('node name ' .. name)
-        name = M.get_node_text(node, bufnr) or ''
-        type_node = node
-      elseif op == 'declaration' or op == 'clause' then
-        declaration_node = node
-        sRow, sCol, eRow, eCol =
-          tsutil.get_vim_range({ vim.treesitter.get_node_range(node) }, bufnr)
-      else
-        debug('unknown op: ' .. op)
-      end
+    debug(query, lang, defaults, pos_row, pos_col)
+    bufnr = bufnr or vim.api.nvim_get_current_buf()
+    local key = tostring(bufnr) .. query
+    local filetime = vim.fn.getftime(vim.fn.expand('%'))
+    if nodes[key] ~= nil and nodestime[key] ~= nil and filetime == nodestime[key] then
+        return nodes[key]
+    end
+    -- todo a huge number
+    pos_row = pos_row or 30000
+    local success, parsed_query = pcall(function()
+        return vim.treesitter.query.parse(lang, query)
     end)
-    if declaration_node ~= nil then
-      debug(name .. ' ' .. op)
-      -- ulog(sRow, pos_row)
-      if sRow > pos_row then
-        debug(tostring(sRow) .. ' beyond ' .. tostring(pos_row))
-        -- break
-      end
-      table.insert(results, {
-        declaring_node = declaration_node,
-        dim = { s = { r = sRow, c = sCol }, e = { r = eRow, c = eCol } },
-        name = name,
-        operator = op,
-        type = node_type or type,
-      })
+    if not success then
+        debug('failed to parse ts query: ' .. query .. 'for ' .. lang)
+        return nil
     end
-    if type_node ~= nil and ntype then
-      debug('type_only')
-      sRow, sCol, eRow, eCol =
-        tsutil.get_vim_range({ vim.treesitter.get_node_range(type_node) }, bufnr)
-      table.insert(results, {
-        type_node = type_node,
-        dim = { s = { r = sRow, c = sCol }, e = { r = eRow, c = eCol } },
-        name = name,
-        operator = op,
-        type = type,
-      })
+
+    local parser = require('nvim-treesitter.parsers').get_parser(bufnr, lang)
+    local root = parser:parse()[1]:root()
+    local start_row, _, end_row, _ = root:range()
+    local results = {}
+    local node_type
+    for match in require('nvim-treesitter.query').iter_prepared_matches(parsed_query, root, bufnr, start_row, end_row) do
+        local sRow, sCol, eRow, eCol
+        local declaration_node
+        local type_node
+        local type = ''
+        local name = ''
+        local op = ''
+        debug(match)
+
+        require('nvim-treesitter.locals').recurse_local_nodes(match, function(_, node, path)
+            -- local idx = string.find(path, ".", 1, true)
+            -- The query may return multiple nodes, e.g.
+            -- (type_declaration (type_spec name:(type_identifier)@type_decl.name type:(type_identifier)@type_decl.type))@type_decl.declaration
+            -- returns { { @type_decl.name, @type_decl.type, @type_decl.declaration} ... }
+            local idx = string.find(path, '.[^.]*$') -- find last `.`
+            op = string.sub(path, idx + 1, #path)
+            local a1, b1, c1, d1 = vim.treesitter.get_node_range(node)
+            local dbg_txt = M.get_node_text(node, bufnr) or ''
+            if #dbg_txt > 100 then
+                dbg_txt = string.sub(dbg_txt, 1, 100) .. '...'
+            end
+            type = string.sub(path, 1, idx - 1) -- e.g. struct.name, type is struct
+            if type:find('type') and op == 'type' then -- type_declaration.type
+                node_type = M.get_node_text(node, bufnr)
+                debug('type: ' .. type)
+            end
+
+            -- stylua: ignore
+            debug(
+            "node ", vim.inspect(node), "\n path: " .. path .. " op: " .. op
+            .. "  type: " .. type .. "\n txt: " .. dbg_txt .. "\n range: " .. tostring(a1 or 0)
+            .. ":" .. tostring(b1 or 0) .. " TO " .. tostring(c1 or 0) .. ":" .. tostring(d1 or 0)
+            )
+            -- stylua: ignore end
+            --
+            -- may not handle complex node
+            if op == 'name' or op == 'value' or op == 'definition' then
+                debug('node name ' .. name)
+                name = M.get_node_text(node, bufnr) or ''
+                type_node = node
+            elseif op == 'declaration' or op == 'clause' then
+                declaration_node = node
+                sRow, sCol, eRow, eCol =
+                tsutil.get_vim_range({ vim.treesitter.get_node_range(node) }, bufnr)
+            else
+                debug('unknown op: ' .. op)
+            end
+        end)
+        if declaration_node ~= nil then
+            debug(name .. ' ' .. op)
+            -- ulog(sRow, pos_row)
+            if sRow > pos_row then
+                debug(tostring(sRow) .. ' beyond ' .. tostring(pos_row))
+                -- break
+            end
+            table.insert(results, {
+                declaring_node = declaration_node,
+                dim = { s = { r = sRow, c = sCol }, e = { r = eRow, c = eCol } },
+                name = name,
+                operator = op,
+                type = node_type or type,
+            })
+        end
+        if type_node ~= nil and ntype then
+            debug('type_only')
+            sRow, sCol, eRow, eCol =
+            tsutil.get_vim_range({ vim.treesitter.get_node_range(type_node) }, bufnr)
+            table.insert(results, {
+                type_node = type_node,
+                dim = { s = { r = sRow, c = sCol }, e = { r = eRow, c = eCol } },
+                name = name,
+                operator = op,
+                type = type,
+            })
+        end
     end
-  end
-  debug('total nodes got: ' .. tostring(#results))
-  nodes[key] = results
-  nodestime[key] = filetime
-  return results
+    debug('total nodes got: ' .. tostring(#results))
+    nodes[key] = results
+    nodestime[key] = filetime
+    return results
 end
 
-M.intersect_nodes = function(nodes, row, col)
-  local found = {}
-  for idx = 1, #nodes do
-    local node = nodes[idx]
-    local sRow = node.dim.s.r
-    local sCol = node.dim.s.c
-    local eRow = node.dim.e.r
-    local eCol = node.dim.e.c
+M.intersect_nodes = function(n, row, col)
+    local found = {}
+    for idx = 1, #n do
+        local node = n[idx]
+        local sRow = node.dim.s.r
+        local sCol = node.dim.s.c
+        local eRow = node.dim.e.r
+        local eCol = node.dim.e.c
 
-    if M.intersects(row, col, sRow, sCol, eRow, eCol) then
-      table.insert(found, node)
+        if M.intersects(row, col, sRow, sCol, eRow, eCol) then
+            table.insert(found, node)
+        end
     end
-  end
 
-  return found
+    return found
 end
 
 M.intersects = function(row, col, sRow, sCol, eRow, eCol)
-  -- ulog(row, col, sRow, sCol, eRow, eCol)
-  if sRow > row or eRow < row then
-    return false
-  end
+    if sRow > row or eRow < row then
+        return false
+    end
 
-  if sRow == row and sCol > col then
-    return false
-  end
+    if sRow == row and sCol > col then
+        return false
+    end
 
-  if eRow == row and eCol < col then
-    return false
-  end
+    if eRow == row and eCol < col then
+        return false
+    end
 
-  return true
+    return true
 end
 
 M.sort_nodes = function(list)
-  table.sort(list, function(a, b)
-    return M.count_parents(a) < M.count_parents(b)
-  end)
-  return list
+    table.sort(list, function(a, b)
+        return M.count_parents(a) < M.count_parents(b)
+    end)
+    return list
 end
 
 M.count_parents = function(node)
-  local count = 0
-  local n = node.declaring_node
-  while n ~= nil do
-    n = n:parent()
-    count = count + 1
-  end
-  return count
+    local count = 0
+    local n = node.declaring_node
+    while n ~= nil do
+        n = n:parent()
+        count = count + 1
+    end
+    return count
 end
 
 
--- nvim/lua/test-runner.lua
+++ nvim/lua/test-runner.lua
@@ -2,52 +2,195 @@
 -- and marks each test accordingly
 local ts = require('go-treesitter')
 
--- local tests = '.'
-local run_tests = function(tests)
-    local tests_regex = table.concat(tests, "|")
+-- make_key is a simple helper to create keys for test state
+local make_key = function(entry)
+    assert(entry.Package, 'Must have Package key: ' .. vim.inspect(entry))
+    assert(entry.Test, 'Must have Test key: ' .. vim.inspect(entry))
+    return string.format('%s/%s', entry.Package, entry.Test)
+end
 
-    vim.cmd('botright vertical new')
-    local buf = vim.api.nvim_get_current_buf()
-    vim.api.nvim_buf_set_lines(buf, 0, 1, false, tests)
-    vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+-- split_tests identifies and separates sub-tests from root tests
+local split_tests = function (test)
+    local parts = {}
+    local n = 1
+    for w in test:gmatch('([^/]*)') do
+        parts[n] = parts[n] or w -- to skip the blanks
+        if w == "" then
+            n = n + 1
+        end
+    end
+    return parts
+end
 
+-- add_test upserts a test to the table. If the test spawns subtests then
+-- the line number will be set to the parent test
+local add_test = function(state, entry)
+    local parts = split_tests(entry.Test)
+    local parent = entry.Test
+    if table.maxn(parts) > 1 then
+        -- Sub-test identified. Use the parent for extmarks
+        parent = parts[1]
+    end
 
-    -- TODO smarter go test building
-    vim.fn.jobstart({ 'go', 'test', '-v', '-json', './...', '-run', tests_regex}, {
-        stdout_buffered = true, -- only send full lines
-        on_stdout = function (_, data)
-            if data then
-                vim.api.nvim_buf_set_lines(buf, -1, 1, false, data)
+    local key = make_key(entry)
+    state.tests[key] = {
+        name = entry.Test,
+        line = ts.get_test_line(state.bufnr, parent),
+        output = {},
+    }
+end
+
+-- add_test_output collects output lines for the given test
+local add_test_output = function(state, entry)
+    assert(state.tests, vim.inspect(state))
+    table.insert(state.tests[make_key(entry)].output, vim.trim(entry.Output))
+end
+
+-- mark_status marks whether the test passed or failed
+local mark_status = function(state, entry)
+    assert(state.tests, vim.inspect(state))
+    state.tests[make_key(entry)].success = (entry.Action == 'pass')
+end
+
+-- run_tests calls out to go test, parses the output, and stores results in given state
+-- tests param can be omitted to run all tests
+local run_tests = function(bufnr, ns, state, tests)
+    -- clear previous extmarks & test state
+    vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
+
+    -- TODO allow for no test to be passed
+    local command = { 'go', 'test', '-v', '-json', './...'}
+    if tests then
+        local tests_regex = table.concat(tests, "|")
+        table.insert(command, '-run')
+        table.insert(command, tests_regex)
+    end
+
+    vim.fn.jobstart(command, {
+        stdout_buffered = true, -- only send full lines on_stdout = function(_, data)
+        on_stdout = function(_, data)
+            if not data then
+                return
+            end
+            for _, line in ipairs(data) do
+                local d = vim.json.decode(line)
+                if d == nil then return end
+
+                if d.Action == 'run' then
+                    add_test(state, d)
+                elseif d.Action == 'output' then
+                    if not d.Test then
+                        return
+                    end
+                    add_test_output(state, d)
+                elseif d.Action == 'pass' or d.Action == 'fail' then
+                    mark_status(state, d)
+
+                    -- Add extmark for test status
+                    local test = state.tests[make_key(d)]
+                    if not test.line or test.line < 0 then
+                        -- No line number found? Don't fail miserably
+                        return
+                    end
+                    if test.success then
+                        vim.api.nvim_buf_set_extmark(bufnr, ns, test.line, 0, { virt_text = {{ "✓" }}})
+                    elseif not test.success then
+                        vim.api.nvim_buf_set_extmark(bufnr, ns, test.line, 0, { virt_text = {{ "x" }}})
+                    end
+                else
+                    print('Skipping unknown line: ' .. vim.inspect(data))
+                end
             end
         end,
-        on_stderr = function(_, data)
-            if data then
-                vim.api.nvim_buf_set_lines(buf, -1, 1, false, data)
+        on_exit = function()
+            -- Add diagnostics for failed tests so we see them in Telescope
+            local failed = {}
+            for _, test in pairs(state.tests) do
+                if test.line then
+                    if not test.success then
+                        table.insert(failed, {
+                            bufnr = bufnr,
+                            lnum = test.line,
+                            col = 0,
+                            severity = vim.diagnostic.severity.ERROR,
+                            source = "go-test",
+                            message = "Test Failed",
+                            user_data = {},
+                        })
+                    end
+                end
             end
+
+            vim.diagnostic.set(ns, bufnr, failed, {})
         end,
     })
 end
 
-local create_cmd = function(cmd, func, opt)
-  opt = vim.tbl_extend('force', { desc = 'test-runner ' .. cmd }, opt or {})
-  vim.api.nvim_create_user_command(cmd, func, opt)
+
+local state = {
+    -- bufnr = bufnr,
+    tests = {},
+}
+
+-- attach_to_buffer manages test state and registering user commands
+local attach_to_buffer = function(bufnr, ns)
+    state.bufnr = bufnr
+
+    -- Run only the test under the cursor
+    vim.api.nvim_buf_create_user_command(bufnr, 'TestFunc', function(_)
+        local n = ts.get_func_method_node_at_pos(bufnr)
+        if n == nil then
+            print('No test func found')
+            return
+        end
+
+        run_tests(bufnr, ns, state, {n.name})
+    end, { desc = 'Run test under cursor' })
+
+    -- TODO need to fix this one's args
+    vim.api.nvim_buf_create_user_command(bufnr, 'TestOnly', function(args)
+        print(args)
+    end, { desc = 'Run specified tests only' })
+
+    -- Get associated Test Output
+    vim.api.nvim_buf_create_user_command(bufnr, "TestOutput", function()
+        local line = 0
+
+        -- try fetching nearest test line num
+        local n = ts.get_func_method_node_at_pos(bufnr)
+        if n == nil then
+            line = vim.fn.line "." - 1
+        else
+            line = ts.get_test_line(bufnr, n.name)
+        end
+
+        -- TODO don't create split if no output
+        -- Create a split for the output
+        vim.cmd('botright vertical new')
+        local buf = vim.api.nvim_get_current_buf()
+        vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+        for _, test in pairs(state.tests) do
+            if test.line == line then
+                vim.api.nvim_buf_set_lines(buf, -1, -1, false, test.output)
+            end
+        end
+    end, { desc = 'Get output for test under cursor' })
 end
 
--- TODO register commands: 
---   TestFunc - run the test the cursor currently sits in / on
---   TestFile - run tests in the given file
---   TestFailed - run only previously-failed tests
---   TestLast - re-run previously-run test commands
-create_cmd('TestFunc', function(_)
-    local buf = vim.api.nvim_get_current_buf()
-    local ns = ts.get_func_method_node_at_pos(buf)
-    if ns == nil then
-        print('No test func found')
-        return
+-- Enable test commands
+local ns = vim.api.nvim_create_namespace('test-runner')
+local group = vim.api.nvim_create_augroup('test-runner', { clear = true })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+    group = group,
+    pattern = '*.go',
+    callback = function()
+        vim.keymap.set('n', '<leader>rt', ':TestFunc<CR>', { desc = 'Run Test under cursor' })
+        vim.keymap.set('n', '<leader>to', ':TestOutput<CR>', { desc = 'Get stored test output' })
+
+        -- Allow state to persist between runs
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        attach_to_buffer(bufnr, ns)
     end
-
-    local func = ns.name
-    run_tests({func})
-end)
-
-vim.keymap.set('n', '<leader>rt', ':TestFunc<CR>', { desc = 'Run Test under cursor' })
+})
