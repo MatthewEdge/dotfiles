a15102b12a2a650c7688f909c4e961cd48d74be6
Matthew Edge <medge@medgelabs.io>
2023-05-21 11:14:01 -0400

    Symlink for alacritty config

--- ln-nvim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Symlink nvim conf to ~/.config/nvim for a better dev experience
-
-rm -rf ~/.config/nvim
-ln -s $(pwd)/nvim ~/.config/nvim
---
+++ symlink-config.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Symlink config folders to ~/.config for a better dev experience
+
+ln -s $(pwd)/nvim ~/.config/nvim
+ln -s $(pwd)/alacritty ~/.config/alacritty
