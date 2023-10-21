fae65c79501e80f38f1677cd4b6b4ccaa5f85eff
Matthew Edge <medge@medgelabs.io>
2023-10-21 08:16:46 -0400

    Fix home ref

--- symlink-config.sh
+++ symlink-config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Symlink config folders to ~/.config for a better dev experience
 
-ln -s $(pwd)/nvim ~/.config/nvim
-ln -s $(pwd)/alacritty ~/.config/alacritty
+ln -s $(pwd)/nvim $HOME/.config/nvim
+ln -s $(pwd)/alacritty $HOME/.config/alacritty
