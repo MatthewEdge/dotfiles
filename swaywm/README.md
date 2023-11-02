fa5cd2179edabcc18cd02f7e4a70ba71d4719a29
Matthew Edge <medge@medgelabs.io>
2023-11-02 09:15:55 -0400

    Add sway config for arch. Update symlink script

---
+++ swaywm/README.md
@@ -0,0 +1,4 @@
+# Sway Window Manager Config
+
+conf under the `/etc` folder is copied system config from Manjaro.
+The rest is for `~/.config/sway` overrides
--- symlink-config.sh
+++ symlink-config.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 # Symlink config folders to ~/.config for a better dev experience
 
+# Sway WM
+rm -rf ~/.config/sway
+ln -s $(pwd)/swaywm $HOME/.config/sway
+rm -rf $HOME/.config/sway/etc
+
+# NVIM
+rm -rf $HOME/.config/nvim
 ln -s $(pwd)/nvim $HOME/.config/nvim
-ln -s $(pwd)/alacritty $HOME/.config/alacritty
+
+# Foot term
+mkdir -p $HOME/.config/foot
+rm -f $HOME/.config/foot/foot.ini
+ln -s $(pwd)/foot.ini $HOME/.config/foot/foot.ini
