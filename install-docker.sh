bfc828afc2d907ecaf8bfe3af1ebfd474054288c
Matthew Edge <medge@medgelabs.io>
2024-03-17 13:24:00 -0400

    Docker now encompases compose

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
 
-sudo $INSTALL docker docker-compose
+sudo $INSTALL docker
 sudo usermod -aG docker $USER
 echo "Make sure to start the docker daemon with: systemctl start docker"
