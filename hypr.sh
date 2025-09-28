4bb772b5ccb558a7d50a5a8a3c778c0dea1a6290
Matthew Edge <medge@medgelabs.io>
2025-09-27 21:59:05 -0400

    Capture hyprland install steps

---
+++ hypr.sh
@@ -0,0 +1,3 @@
+sudo pacman -Syu hyprland xdg-desktop-portal-hyprland dunst hyprpolkitagent qt5-wayland qt6-wayland waybar hyprpaper rofi-wayland hyprpicker hyprlock thunar ffmpegthumbnailer hyprshot xf86-video-amdgpu mesa lib32-mesa vulkan-radeon
+sudo pacman -Syu git ripgrep fzf noto-fonts noto-fonts-emoji pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack
+sudo pacman -Syu firefox obs-studio audacity vlc
