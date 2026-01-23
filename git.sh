cec1d0b427d6c145afdf4c9d2426b441d33e67a7
Matthew Edge <medge@medgelabs.io>
2026-01-23 09:16:55 -0500

    Git fix

--- git.sh
+++ git.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Git config
-git config --global user.name "Matthew Edge"
+git config --global user.name "Medge"
 git config --global user.email "medge@medgelabs.io"
 git config --global core.pager 'cat'
 git config --global init.defaultBranch main
