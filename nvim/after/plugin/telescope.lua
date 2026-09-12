789506f575f93cc412087a4114a766bccb3729f3
Medge <medge@medgelabs.io>
2026-09-12 20:14:41 +0200

    Colocate aerial with telescope config

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -42,3 +42,17 @@ vim.keymap.set('n', '<leader>fb', ts.buffers, { desc = 'Find Buffers' })
 vim.keymap.set('n', '<leader>fh', ts.help_tags, { desc = 'Find Help Tags' })
 vim.keymap.set('n', '<leader>km', ts.keymaps, { desc = 'Key Mappings' })
 vim.keymap.set('n', '<leader>pd', ts.diagnostics, { desc = 'Project Diagnostics' })
+
+require('aerial').setup({
+    on_attach = function (bufnr)
+        vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', {buffer = bufnr})
+        vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', {buffer = bufnr})
+    end,
+
+    layout = {},
+    close_on_select = false,
+})
+
+-- Register Telescope extension
+require('telescope').load_extension('aerial')
+vim.keymap.set('n', '<leader>ss', require('telescope').extensions.aerial.aerial, {desc = '[S]how [S]ymbols'})
